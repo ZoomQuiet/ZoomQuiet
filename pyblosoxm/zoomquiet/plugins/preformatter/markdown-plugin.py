f0f5a5f7812afec056094ddaf45c8ea49de08c41
Zoom.Quiet <zoomquiet+hg@gmail.com>
2012-03-19 16:10:50 +0800

    rsync pyb. into zsp hosing (a) 2012/03/19 16:10:50 {by} pyb4pub2sora.sh v12.03.06

--- pyblosoxm/zoomquiet/plugins/preformatter/markdown-plugin.py
+++ pyblosoxm/zoomquiet/plugins/preformatter/markdown-plugin.py
@@ -4,17 +4,17 @@ markdown-plugin.py -- A Python Markdown v2.x plugin for PyBlosxom.
 
 This plugin requires Python Markdown v2.x, which you can download from:
 
-	http://www.freewisdom.org/projects/python-markdown/
+    http://www.freewisdom.org/projects/python-markdown/
 
 Extract the 'markdown' directory from the Python Markdown tarball (the
 directory containing __init__.py, not the Markdown-2.x.y directory)  into your
 pyblosxom plugins dir alongside this plugin. Your plugins dir should look like
 this:
 
-	plugins/ <-- your pyblosxom plugins dir
-		markdown-plugin.py <-- this file
-		markdown/ <-- the Python Markdown module
-		... <-- (any other pyblosxom plugins)
+    plugins/ <-- your pyblosxom plugins dir
+        markdown-plugin.py <-- this file
+        markdown/ <-- the Python Markdown module
+        ... <-- (any other pyblosxom plugins)
 
 Now any posts with filenames ending in one of the FILENAME_EXTENSIONS defined
 below will be passed through python-markdown.
@@ -40,8 +40,10 @@ import os
 md = markdown.Markdown(output_format='html4',extensions=['extra',])
 
 def cb_story(args):
-	entry = args['entry']
-	if os.path.splitext(entry['filename'])[1] in FILENAME_EXTENSIONS:
-		entry['body'] = md.convert(u''.join(entry['body'].decode("utf-8")))
-		md.reset()
-	return args
+    entry = args['entry']
+    print os.path.splitext(entry['filename'])
+    if os.path.splitext(entry['filename'])[1] in FILENAME_EXTENSIONS:
+        entry['body'] = md.convert(u''.join(entry['body'].decode("utf-8")))
+        md.reset()
+    return args
+
